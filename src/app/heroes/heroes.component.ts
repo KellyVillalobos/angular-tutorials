aa514cfa513c08a687917aacaa011bcba6d278fe
Kelly Villalobos <kvdesign1978@gmail.com>
2018-07-25 23:19:50 -0500

    finished the routing and services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +10,18 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
-
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location
+  ) { }
 
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-
-  }
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
